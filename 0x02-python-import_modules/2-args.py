c4a518c533eb36d2861ed79e82b5b6e11bdb2e7d
Emmy <softdevcoder@gmail.com>
2022-07-01 04:29:30 -0400

    added a fix for lenght variable

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    length = len(sys.argv) - 1
-    if length == 0:
-        print("{} arguments.".format(length))
-    elif length == 1:
-        print("{} arguments:".format(length))
+    i = len(sys.argv) - 1
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} arguments:".format(i))
     else:
-        print("{} arguments:".format(length))
+        print("{} arguments:".format(i))
 
-    if length >= 1:
+    if i >= 1:
         i = 0
         for arg in sys.argv:
             if i != 0:
