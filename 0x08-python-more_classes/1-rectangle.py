0da43f6d3e425f9d01b9694435f0f85d8708e87c
Emmy <softdevcoder@gmail.com>
2022-07-25 11:04:16 -0400

    fix the private instance attribute

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,37 +1,47 @@
 #!/usr/bin/python3
+
 '''A class about rectangle'''
 
 
 class Rectangle:
     '''A rectangle class'''
     def __init__(self, width=0, height=0):
-        '''Initialization of class'''
-        pass
+        '''Initialization of class
+        
+        Args:
+            width: width of the Rectangle
+            height: height of the Rectangle
+        '''
+        self.width = width
+        self.height = height
+
 
     @property
     def width(self):
         '''a getter attribute'''
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
         '''a setter attribute'''
-        if is not isinstance(value, int):
+        if not isinstance(value, int):
             return TypeError("width must be an integer")
         elif value < 0:
             return ValueError("width must be >= 0")
         else:
-            self.width = value
+            self.__width = value
 
     @property
     def height(self):
         """a getter attribute for height"""
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
         """a setter attribute for height"""
-        if is not isinstance(value, int):
+        if not isinstance(value, int):
             return TypeError("height must be an integer")
         elif value < 0:
             return ValueError("")
+        else:
+            self.__height = value
