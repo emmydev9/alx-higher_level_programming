c2787ba0e5302fb436fd2e030aff01a4ace2e85f
Emmy <softdevcoder@gmail.com>
2022-08-02 16:11:01 -0400

    pycodestyle fix

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,11 +4,11 @@
 
 import json
 
-def to_json_string(my_obj):
-	""" function that return as a json dumps of an obj
-	
-	Args: 
-		my_obj: obj
 
-	"""
-	return json.dumps(my_obj)
+def to_json_string(my_obj):
+    """ function that return as a json dumps of an obj
+    Args:
+        my_obj: obj
+
+    """
+    return json.dumps(my_obj)
