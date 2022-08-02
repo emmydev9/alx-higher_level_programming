7d9b37c88dc7c7253d75c2afd910dca00f539422
Emmy <softdevcoder@gmail.com>
2022-08-02 17:00:53 -0400

    [feat] create an json_obj from a file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module that contains a function that
+creates an Object from a “JSON file”"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """A function that creates an Object from a “JSON file”"""
+
+    with open(filename, "r", encoding="utf-8") as f:
+        json.load(filename, f)
