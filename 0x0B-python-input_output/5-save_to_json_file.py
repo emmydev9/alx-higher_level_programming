da7ac01728d0016ddc88fae0cf62ce52a37b84df
Emmy <softdevcoder@gmail.com>
2022-08-02 16:38:11 -0400

    [Feat] write a json to a file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A module that contains a function that writes
+ an Object to a text file, using a JSON representation"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    s_my_obj = json.dumps(my_obj)
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(s_my_obj)
