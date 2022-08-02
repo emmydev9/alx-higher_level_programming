c3a217775b76046992fb54b7d5b9ada1c87b6dc8
Emmy <softdevcoder@gmail.com>
2022-08-02 16:08:58 -0400

    [Feat] serialize a given obj

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A module that contains a function that serialize object"""
+
+
+import json
+
+def to_json_string(my_obj):
+	""" function that return as a json dumps of an obj
+	
+	Args: 
+		my_obj: obj
+
+	"""
+	return json.dumps(my_obj)
