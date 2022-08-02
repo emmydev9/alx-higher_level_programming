f4400018152157493dfe7fe54af3ff4ca86f7ed5
Emmy <softdevcoder@gmail.com>
2022-08-02 16:40:04 -0400

    fix docstring

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,6 +6,8 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """a function that writes
+    an Object to a text file, using a JSON representation"""
     s_my_obj = json.dumps(my_obj)
     with open(filename, "w", encoding="utf-8") as f:
         f.write(s_my_obj)
