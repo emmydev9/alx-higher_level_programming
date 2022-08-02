d38e2f87ce47026d009289afcd049da2e9967d45
Emmy <softdevcoder@gmail.com>
2022-08-02 12:13:36 -0400

    added a docs

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""" module that contains function which can read from a file """
+
 
 def read_file(filename=""):
+    """A function that can read from a file"""
+
     with open(filename, encoding="utf-8") as f:
         read = f.read()
     print(read)
