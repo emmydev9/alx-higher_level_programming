f69fb38768265ecc5abe2f5038a586d7b69eacf8
Emmy <softdevcoder@gmail.com>
2022-09-06 17:25:35 -0400

    added a script that lists all records of a table with name not NULL

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- script that lists all records of the table 
+-- A query that lists all records of a table with name not NULL
+SELECT score, name FROM second_table WHERE name NOT NULL ORDER BY score DESC;
