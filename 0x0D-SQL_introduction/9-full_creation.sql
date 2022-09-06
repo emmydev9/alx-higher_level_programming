daf2141ba83b4db10e3ecd8a87e79fb9121c2187
Emmy <softdevcoder@gmail.com>
2022-09-06 11:32:47 -0400

    added a fix for multiple columns

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,7 @@
 -- Create a field and Records in a table
 -- A query to create a rows and column in with a new table
-CREATE TABLE IF NOT EXISTS second_table(id INT DEFAULT NULL, name VARCHAR(256) DEFAULT NULL, score INT DEFAULT NULL);
-INSERT INTO second_table(id, name, score) VALUES ((1, "John", 10), (2, "Alex", 3), (3, "Bob", 14),
-(4, "George", 8));
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table(id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table(id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table(id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table(id, name, score) VALUES (4, "George", 8);
