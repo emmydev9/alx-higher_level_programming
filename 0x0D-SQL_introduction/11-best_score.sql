2a0f51949ddfcf3ae0ef795923bdb2245b034ff7
Emmy <softdevcoder@gmail.com>
2022-09-06 15:12:10 -0400

    added a fix

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- A script that displays lists all records with a score >= 10
 -- A query that lists all records
-SELECT score, name FROM second_table WHERE score >= 10 DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
