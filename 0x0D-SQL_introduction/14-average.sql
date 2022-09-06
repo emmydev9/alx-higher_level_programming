ec94c970bef71d6c6095a64da953735805810a5d
Emmy <softdevcoder@gmail.com>
2022-09-06 16:16:30 -0400

    added a fix

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- script that computes the score average of all records
 -- A query that computes the score of average.
-SELECT AVG(average) FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
