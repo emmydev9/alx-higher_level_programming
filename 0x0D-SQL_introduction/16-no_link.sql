cc210292a271e400502bb0785ca4babb6c724c48
Emmy <softdevcoder@gmail.com>
2022-09-06 17:28:09 -0400

    added a fix

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- script that lists all records of the table 
 -- A query that lists all records of a table with name not NULL
-SELECT score, name FROM second_table WHERE name != NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
