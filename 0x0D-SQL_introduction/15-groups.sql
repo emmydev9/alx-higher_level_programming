fffe39c9031817454054421c0157b52335397a2d
Emmy <softdevcoder@gmail.com>
2022-09-06 17:18:04 -0400

    added descending order

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- lists the number of records with the same score in the table
 -- A query that list the number of records with the same score
-SELECT score, count(*) AS number FROM second_table GROUP BY score ORDER BY score;
+SELECT score, count(*) AS number FROM second_table GROUP BY score ORDER BY score DESC;
