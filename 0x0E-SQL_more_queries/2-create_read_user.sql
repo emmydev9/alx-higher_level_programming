d1e026d042600e6aa024269d5e0af755afd04342
Emmy <softdevcoder@gmail.com>
2022-09-07 07:23:13 -0400

    added a fix for the password

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- create a database and a user if not exist
 -- assign a privilege of only that particular database
-CREATE DATABASE iF NOT EXISTS hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2;
-GRANT SELECT hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFiED BY 'user_0d_2_pwd';
