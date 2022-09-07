7feb3921535955208e5c42a7f59986adf45472ea
Emmy <softdevcoder@gmail.com>
2022-09-07 16:52:55 -0400

    added a fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- create a new user if not exist
 -- and grant full permission to the user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY "user_0d_1_pwd";
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
