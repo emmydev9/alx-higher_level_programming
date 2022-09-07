9ca9b6d0baaddc93a92bcfc499dbb0decedfef08
Emmy <softdevcoder@gmail.com>
2022-09-07 07:21:55 -0400

    [Feature] a script that create a new database, new user and grants select privileges

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- create a database and a user if not exist
+-- assign a privilege of only that particular database
+CREATE DATABASE iF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2;
+GRANT SELECT hbtn_0d_2.* TO 'user_0d_2'@'localhost';
