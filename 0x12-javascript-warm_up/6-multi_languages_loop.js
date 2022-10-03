40c33ca96a8ae9952656179f4c72e099708474f3
Emmy <softdevcoder@gmail.com>
2022-10-03 12:08:33 -0400

    added a semistandard code style fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
 for (let i = 0; i < 1; i++) {
-  console.log('C is fun' + '\n' + "Python is cool" + '\n' + "JavaScript is amazing");
+  console.log('C is fun' + '\n' + 'Python is cool' + '\n' + 'JavaScript is amazing');
 }
