aa9a23721fad562d6f365baebaead38817c77dd2
Emmy <softdevcoder@gmail.com>
2022-10-03 12:07:27 -0400

    added a fix for single console.log

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
 for (let i = 0; i < 1; i++) {
-  console.log('C is fun');
-  console.log('Python is cool');
-  console.log('JavaScript is amazing');
+  console.log('C is fun' + '\n' + "Python is cool" + '\n' + "JavaScript is amazing");
 }
