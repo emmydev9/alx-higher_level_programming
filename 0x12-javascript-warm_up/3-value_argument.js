7865a585998d478230348f64b8439fb5c82c6d75
Emmy <softdevcoder@gmail.com>
2022-10-03 11:39:09 -0400

    [fix] semistandard code style

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-const process = require("process")
-let len = 0
+const process = require('process');
+let len = 0;
 process.argv.forEach(() => {
-	len += 1
-})
+  len += 1;
+});
 
 if (len < 3) {
-	console.log('No argument')
+  console.log('No argument');
 } else {
-	console.log(process.argv[2])
+  console.log(process.argv[2]);
 }
