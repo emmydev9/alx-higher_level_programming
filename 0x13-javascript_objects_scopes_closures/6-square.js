68adccdd9bd0da77d44fac8dd4f2e53bb21799c3
Emmy <softdevcoder@gmail.com>
2022-10-04 14:40:42 -0400

    [Feat] prints a character or x

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const Squares = require('./5-square.js');
+
+class Square extends Squares {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (!c) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.size; i++) {
+        let x = '';
+        for (let j = 0; j < this.size; j++) {
+          x += c;
+        }
+        console.log(x);
+      }
+    }
+  }
+}
+
+module.exports = Square;
