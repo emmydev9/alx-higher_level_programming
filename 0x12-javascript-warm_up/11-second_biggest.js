d022945df8af2d91ce7874fdc10f68079089bd9e
Emmy <softdevcoder@gmail.com>
2022-10-04 03:45:32 -0400

    added a fix for export and second largest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,11 +3,15 @@
 const len = process.argv.length;
 const arr_ = [];
 
+function compare (a, b) {
+  return (a - b);
+}
+
 if (len < 4) {
   console.log(0);
 } else {
   for (let i = 2; i < len; i++) {
     arr_.push(process.argv[i]);
   }
-  console.log((arr_.sort()[len - 4]));
+  console.log((arr_.sort(compare)[len - 4]));
 }
