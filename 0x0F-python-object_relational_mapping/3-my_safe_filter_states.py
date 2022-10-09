d63582cdad6d9d5edac3ee6aabb6903b51e700d0
Emmy <softdevcoder@gmail.com>
2022-10-09 10:58:21 -0400

    added a safe search script

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" search a states from a database in a safe way"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    p = sys.argv[2]
+    db = sys.argv[3]
+    search = sys.argv[4]
+    port = 3306
+
+    db = MySQLdb.connect(host="localhost", user=u, passwd=p, db=db, port=port)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}';".format(search))
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
