e4a07adfa000f1d4f47dcf9c2bbb55c75f9b0cef
Emmy <softdevcoder@gmail.com>
2022-10-20 12:11:18 -0400

    added command argument

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that list the supported http method on a server
-curl -sI -X OPTIONS | grep "Allow:" | cut -d " " -f 2-
+curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
