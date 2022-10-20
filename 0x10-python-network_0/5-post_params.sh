909fdb311f31725cb96ba249f5c2586638855252
Emmy <softdevcoder@gmail.com>
2022-10-20 12:32:09 -0400

    added a follow redirect option

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a data with POST request
-curl -s -X POST "$1" -d '{"email": "test@gmail.com", "subject": "I will always be here for PLD"}'
+curl -Ls -X POST "$1" -d '{"email": "test@gmail.com", "subject": "I will always be here for PLD"}'
