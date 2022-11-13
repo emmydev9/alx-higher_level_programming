9edc9f686495561ae71caf34029b91bcdb4ece27
Emmy <softdevcoder@gmail.com>
2022-11-12 19:48:54 -0500

    moved files to the correct directory

---
+++ 0x04-python-more_data_structures/test.py
@@ -0,0 +1,9 @@
+def common_elements(set_1, set2):
+	return (set_1 & set_2)
+
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+c_set = common_elements(set_1, set_2)
+
+common_elements(set_1, set_2)
+print(sorted(list(c_set)))
