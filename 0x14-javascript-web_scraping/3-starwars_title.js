ef523eb130e86f43bbcb6f838a82e0ef0114cdb8
Emmy <softdevcoder@gmail.com>
2022-11-12 21:34:40 -0500

    added a fix for the page url

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const id = process.argv[2];
-request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
+request('https://swapi-api.hbtn.io/api/films/' + id + '/', function (error, response, body) {
   if (error == null) {
     const json = JSON.parse(body);
     console.log(json.title);
