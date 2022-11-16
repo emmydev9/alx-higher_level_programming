688aab45caa8355b136f3c34261f5fed5efaf217
Emmy <softdevcoder@gmail.com>
2022-11-16 13:42:25 -0500

    added a url fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/' + process.argv[2];
 
 request(url, function (err, response, body) {
   if (err == null) {
